7cd4317827ab89f5653b01f9f294a892eddf22ed
liushu <ls@liushudeMacBook-Pro.local>
2015-08-25 17:49:32 +0800

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by liushu on 15/8/25.
+//  Copyright (c) 2015年 liushu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
